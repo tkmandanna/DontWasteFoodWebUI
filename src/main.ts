adcaaaab7830057df030848412ef05ee19de9d84
Mandanna Thekkada <mandanna.thekkada@gmail.com>
2023-08-28 16:55:46 -0400

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule) //this is the first module that will be loaded 
   .catch(err => console.error(err));
